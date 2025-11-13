163272e7f651a0cc0d29c512aa41f564ab6f59b6
Adrihan249 <adrihanmendoza2018@gmail.com>
2025-11-13 11:25:12 -0500

    Update vite.config.js nufeajnlds

--- fronten/turismo-sostenible/vite.config.js
+++ fronten/turismo-sostenible/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  
+  // ⬇️ SECCIÓN A AGREGAR ⬇️
+  preview: {
+    // Esto permite que Vite acepte solicitudes desde el dominio de Render.
+    allowedHosts: [
+      'turismo-sostenible-inteligente.onrender.com'
+    ]
+  }
+});
